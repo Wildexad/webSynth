d81f6d58c77bbf2fab8ab61d1b7af71606320665
voldezhur <voldezhur@gmail.com>
2024-04-17 18:29:16 +0300

    Added buttons to InstrumentList that change activeSynth

--- client/src/components/InstrumentList.jsx
+++ client/src/components/InstrumentList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 const InstrumentList = ({ setActiveSynth, synthList, setSynthList }) => {
-    
-    
     return (
         <>
-            
+            <ul>
+                {
+                    synthList.map(synth => <li>
+                        <button onClick={() => {setActiveSynth(synth)}}>
+                            {synth.name}
+                        </button>
+                    </li>)
+                }
+            </ul>
         </>
     );
 }
